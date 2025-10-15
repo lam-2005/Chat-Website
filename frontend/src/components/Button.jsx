535015ca0d32bbfc7f387dccf6b36a334cb72fd7
lam-2005 <lam2k5phan@gmail.com>
2025-10-15 18:37:26 +0700

    layout signup page completed

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Button = ({ className, type, onClick, children }) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`bg-gradient-to-l from-orange-400  to-pink-400 p-2 text-white rounded-xl hover:scale-105 duration-200 hover:drop-shadow-2xl hover:shadow-black/50 cursor-pointer
+        flex items-center justify-center
+        ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,73 +1,86 @@
 import React from "react";
 import useAuthStore from "../store/useAuthStore";
 import { LoaderIcon } from "lucide-react";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import { Link } from "react-router";
 
 const SignUp = () => {
   const [formData, setFormData] = React.useState({
     userName: "",
     email: "",
     password: "",
+    confirmPassword: "",
   });
 
   const { signup, isSigningUp } = useAuthStore();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     signup(formData);
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="">Full Name</label>
-          <div>
-            <input
-              className="input input-bordered input-primary"
-              type="text"
-              value={formData.userName}
-              onChange={(e) =>
-                setFormData({ ...formData, userName: e.target.value })
-              }
-            />
-          </div>
-        </div>
+    <div
+      className="bg-card w-full max-w-[450px]  absolute top-1/2 left-1/2 -translate-1/2
+    shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] rounded-2xl bg-white p-5 flex gap-4 flex-col
+    "
+    >
+      <h1 className="text-4xl text-pink-600">WELCOME!</h1>
+      <div className="w-1/2 bg-gradient-to-l from-orange-400  to-pink-300 h-0.5 self-end"></div>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-8 mt-4">
+        <Input
+          label={"Fullname"}
+          value={formData.userName}
+          name={"userName"}
+          id={"username"}
+          onChange={handleChange}
+        />
+        <Input
+          label={"Email"}
+          value={formData.email}
+          name={"email"}
+          id={"email"}
+          onChange={handleChange}
+        />
+        <Input
+          label={"Password"}
+          value={formData.password}
+          name={"password"}
+          id={"password"}
+          onChange={handleChange}
+          type="password"
+        />
+        <Input
+          label={"Comfirm Password"}
+          onChange={handleChange}
+          name={"confirmPassword"}
+          value={formData.confirmPassword}
+          type="password"
+          id={"confirmPassword"}
+        />
 
-        <div>
-          <label htmlFor="">Email</label>
-          <div>
-            <input
-              className="input input-bordered input-primary"
-              type="text"
-              value={formData.email}
-              onChange={(e) =>
-                setFormData({ ...formData, email: e.target.value })
-              }
-            />
-          </div>
-        </div>
-
-        <div>
-          <label htmlFor="">Password</label>
-          <div>
-            <input
-              className="input input-bordered input-primary"
-              type="text"
-              value={formData.password}
-              onChange={(e) =>
-                setFormData({ ...formData, password: e.target.value })
-              }
-            />
-          </div>
-        </div>
-
-        <button className="btn btn-primary" type="submit">
+        <Button className={"text-xl"}>
           {isSigningUp ? (
             <LoaderIcon className="w-full h-5 animate-spin text-center" />
           ) : (
-            "Create Account"
+            <span>SIGN UP!</span>
           )}
-        </button>
+        </Button>
       </form>
+      <div className="flex gap-1 mt-5 self-center">
+        <p>Already have an account?</p>
+        <Link to={"/login"} className="text-pink-600 hover:underline font-bold">
+          SIGN IN
+        </Link>
+      </div>
     </div>
   );
 };
