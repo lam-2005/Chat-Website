a0d70ca57ea0e7c44f0f626f5e37f55b52de392b
lam-2005 <lam2k5phan@gmail.com>
2025-10-19 15:09:09 +0700

    navbar layout completed

--- frontend/src/components/MessageLoadingSkeleton.jsx
+++ frontend/src/components/MessageLoadingSkeleton.jsx
@@ -8,7 +8,7 @@ const MessageLoadingSkeleton = () => {
             i % 2 === 0 ? "chat-start" : "chat-end"
           } animate-pulse m-0`}
         >
-          <div className={`chat-bubble w-32 bg-gray-200 text-white`}></div>
+          <div className={`chat-bubble w-32 bg-black/10 text-white`}></div>
         </div>
       ))}
     </div>
